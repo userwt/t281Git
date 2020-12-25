c362e5e3b87ae27e00aaa45b4a97483b5b9cf9b4
18373457821tgh <2380499075@qq.com>
2020-12-25 15:20:09 +0800

    15：19

---
+++ src/com/yidu/bean/shuabi.java
@@ -0,0 +1,13 @@
+package com.yidu.bean;
+
+/**
+ * @author 胖迪
+ * @version 1.0
+ * @description:
+ * @2020/12/25 15:18
+ */
+public class shuabi {
+    public static void main(String[] args) {
+        System.out.println("靓仔");
+    }
+}
